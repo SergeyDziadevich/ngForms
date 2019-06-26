a3534ac8d4a9d4fda8193e86996b9223d717b83a
Sergey Dziadevich <sergey_dziadevich@epam.com>
2019-06-26 16:47:36 +0300

    Custom Validator Directive w/ Parameters

--- src/app/validators/service-level.directive.ts
+++ src/app/validators/service-level.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, Input } from '@angular/core';
 import { Validator, AbstractControl, NG_VALIDATORS } from '@angular/forms';
 
 import { checkServiceLevel } from './custom.validators';
@@ -6,14 +6,16 @@ import { checkServiceLevel } from './custom.validators';
 @Directive({
   selector: '[appServiceLevelValidator]',
   providers: [{
-      provide: NG_VALIDATORS,
-      useExisting: ServiceLevelDirective,
-      multi: true
+    provide: NG_VALIDATORS,
+    useExisting: ServiceLevelDirective,
+    multi: true
   }]
 })
 export class ServiceLevelDirective implements Validator {
+  @Input() rMin = 1;
+  @Input() rMax = 3;
 
   validate(c: AbstractControl): { [key: string]: boolean } | null {
-    return checkServiceLevel(c, 1, 3);
+    return checkServiceLevel(c, this.rMin, this.rMax);
   }
 }
